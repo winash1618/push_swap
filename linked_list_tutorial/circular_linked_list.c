d24e9b9064252fabf005fea511edf5b723202404
asharaf <51042907+winash1618@users.noreply.github.com>
2022-03-21 08:15:59 +0400

    divided init_num_list to two functions

--- linked_list_tutorial/circular_linked_list.c
+++ linked_list_tutorial/circular_linked_list.c
@@ -50,6 +50,8 @@ void push_circular(new_num** head_ref, int new_data)
 	/* 4. move the head to point to the new node */
 	(*head_ref) = new_node;
 }
+// initialising the structure.
+
 new_num *init_num_list(char **arg)
 {
 	new_num *num_last;
@@ -83,8 +85,44 @@ new_num *init_num_list(char **arg)
 	num->prev_num = num_last;
 	return (num);
 }
+void other_nums(char **arg, new_num **num_first, new_num **num_last, new_num **num)
+{
+	int i;
+	new_num *num_next;
+
+	i = 2;
+	while (arg[i] != NULL)
+	{
+		num_next = malloc(sizeof(new_num));
+		num_next->data = ft_atoi(arg[i]);
+		num_next->next_num = *num_first;
+		(*num)->next_num = num_next;
+		*num_last = *num;
+		*num = (*num)->next_num;
+		(*num)->prev_num = *num_last;
+		i++;
+	}
+}
+new_num *set_num_list(char **arg)
+{
+	new_num *num_last;
+	new_num *num_first;
+	new_num *num = malloc(sizeof(new_num));
+
+	if (!num)
+		return (NULL);
+	num->data = ft_atoi(arg[1]);
+	num->prev_num = num;
+	num->next_num = num;
+	num_first = num;
+	other_nums(arg, &num_first, &num_last, &num);
+	num_last = num;
+	num = num->next_num;
+	num->prev_num = num_last;
+	return (num);
+}
 int main(int ac, char **av)
 {
-	new_num *me = init_num_list(av);
+	new_num *me = set_num_list(av);
 	printf("%d", (me->next_num)->data);
 }

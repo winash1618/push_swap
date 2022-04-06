5db75bdb8b6bcc9c1a81d599945e96e0f4e51e30
asharaf <51042907+winash1618@users.noreply.github.com>
2022-04-06 08:12:08 +0400

    corrected leaks

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mkaruvan <mkaruvan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 08:56:40 by mkaruvan          #+#    #+#             */
-/*   Updated: 2022/04/06 07:24:21 by mkaruvan         ###   ########.fr       */
+/*   Updated: 2022/04/06 08:10:55 by mkaruvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,27 @@ static int	count_nums(char **av)
 	return (--i);
 }
 
-static void	ft_free(t_new **me)
+static void	ft_free(t_new **me, char **av, int flag)
 {
 	t_new	*first;
+	int		i;
 
+	i = 1; // i had to start from 1 since i did not allocate av[0]
 	while (*me != NULL)
 	{
 		first = *me;
 		delete_cond(me);
 		free(first);
+	}
+	if (flag)
+	{
+		
+		while (av[i] != NULL)
+		{
+			free(av[i]);
+			i++;
+		}
+		free(av);
 	}
 }
 
@@ -54,8 +66,23 @@ static char	**splited(char **av)
 		av[j + 1] = ag[j];
 		j++;
 	}
+	free(ag);
 	av[j + 1] = NULL;
 	return (av);
+}
+
+static void	push_swap(t_new **me, int len)
+{
+	t_new	*lst;
+
+	lst = NULL;
+	if (!is_duplicate(*me, len))
+	{
+		if (!is_sorted_list(*me, len) && len > 5)
+			big_sort(me, &lst);
+		else if (!is_sorted_list(*me, len))
+			five_sort(me, &lst, len);
+	}
 }
 
 int	main(int ac, char **av)
@@ -63,26 +90,20 @@ int	main(int ac, char **av)
 	int		len;
 	int		flag;
 	t_new	*me;
-	t_new	*lst;
 
+	flag = 0;
 	len = ac - 1;
 	if (len == 1)
 	{
 		av = (char **)splited(av);
 		len = count_nums(av);
+		flag = 1;
 	}
 	if (is_string_number(av, len) && len > 0 && is_integer(av, len))
 	{
 		me = set_num_list(av);
 		rank_list(len, av, &me);
-		lst = NULL;
-		if (!is_duplicate(me, len))
-		{
-			if (!is_sorted_list(me, len) && len > 5)
-				big_sort(&me, &lst);
-			else if (!is_sorted_list(me, len))
-				five_sort(&me, &lst, len);
-		}
+		push_swap(&me, len);
 		ft_free(&me, av, flag);
 	}
 	return (0);
